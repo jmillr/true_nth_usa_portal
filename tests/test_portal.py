4d7df33ffb82bcdd3757275487942273a41756b9
Paul Bugni <pbugni@u.washington.edu>
2017-07-17 15:27:21 -0700

    Refactor /settings endpoints to their own module.

--- tests/test_portal.py
+++ tests/test_portal.py
@@ -219,14 +219,6 @@ class TestPortal(TestCase):
             urllib.urlencode(params)))
         self.assert200(rv)
 
-    def test_configuration_settings(self):
-        self.login()
-        lr_group = self.app.config['LR_GROUP']
-        rv = self.client.get('/api/settings/lr_group')
-        self.assert200(rv)
-        self.assertEquals(rv.json.get('LR_GROUP'), lr_group)
-        rv2 = self.client.get('/api/settings/bad_value')
-        self.assertEquals(rv2.status_code, 400)
 
     def test_redirect_validation(self):
         self.promote_user(role_name=ROLE.ADMIN)
