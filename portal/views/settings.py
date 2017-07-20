152fa099dad01e9c4cf1ab253f0553a6057d5036
Paul Bugni <pbugni@u.washington.edu>
2017-07-20 14:28:44 -0700

    Add long lived browser cookie for kiosk force_reauth of IdPs.

--- portal/views/settings.py
+++ portal/views/settings.py
@@ -2,7 +2,7 @@ from flask import abort, make_response, request
 from flask import current_app, Blueprint, jsonify, render_template
 from flask_user import roles_required
 from flask_wtf import FlaskForm
-from wtforms import validators, IntegerField
+from wtforms import validators, BooleanField, IntegerField
 
 from ..extensions import oauth
 from ..models.app_text import app_text, AppText
@@ -13,8 +13,11 @@ settings_api = Blueprint('settings', __name__)
 
 
 class SettingsForm(FlaskForm):
-    timeout = IntegerField('Session Timeout for This Web Browser (in seconds)',
-                           validators=[validators.Required()])
+    timeout = IntegerField(
+        'Session Timeout for This Web Browser (in seconds)',
+        validators=[validators.Required()])
+    force_reauth = BooleanField(
+        'Force re-authentication from external Identity Providers')
 
 
 @settings_api.route('/settings', methods=['GET', 'POST'])
@@ -36,7 +39,8 @@ def settings():
             apptext[a.name] = a.custom_text
 
     form = SettingsForm(
-        request.form, timeout=request.cookies.get('SS_TIMEOUT', 600))
+        request.form, timeout=request.cookies.get('SS_TIMEOUT', 600),
+        force_reauth=request.cookies.get('FORCE_REAUTH', False))
     if not form.validate_on_submit():
 
         return render_template(
@@ -55,6 +59,11 @@ def settings():
         organization_consents=organization_consents,
         wide_container="true"))
     response.set_cookie('SS_TIMEOUT', str(form.timeout.data), max_age=max_age)
+    if form.force_reauth.data:
+        response.set_cookie('FORCE_REAUTH', 'True', max_age=max_age)
+    else:
+        # close as we can come to deleting a cookie
+        response.set_cookie('FORCE_REAUTH', '', expires=0)
     return response
 
 
